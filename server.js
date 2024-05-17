e11aca83f8d2867bdf69e2f8b029074a91bb1c44
Shailendra Bhargava <shailendrabhargavaa@gmail.com>
2024-05-17 18:32:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const app = express();
 const port = process.env.PORT || 8000;
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const swaggerUI = require("swagger-ui-express");
+const swaggerSpec = require("./swagger");
+
 //setup firebase
 var admin = require("firebase-admin");
 var serviceAccount = require("./serviceAccountKey.json");
@@ -15,7 +18,12 @@ admin.initializeApp({
 //database reference
 let db = admin.firestore();
 
+// Serve Swagger documentation
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
+
 require("./app/routes")(app, db);
+
+app.get("/", () => res.send("App is running !!"));
 
 app.listen(port, () => console.info(`Running on ${port}`));
 
